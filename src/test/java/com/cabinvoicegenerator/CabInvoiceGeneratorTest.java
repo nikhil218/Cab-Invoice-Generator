df542de00cd1fe9e59f583796c850ab983d456f8
nikhil tirpude <nikhil.tirpude317@gmail.com>
2021-05-28 00:10:11 +0530

    written code with logic to calculate cab fare

---
+++ src/test/java/com/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,27 @@
+package com.cabinvoicegenerator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+
+class CabInvoiceGeneratorTest {
+CabInvoiceGenerator cabInvoiceGenerator;
+    @BeforeEach
+    void setUp() {
+        cabInvoiceGenerator = new CabInvoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndTimeInvoiceGeneratorShouldReturnTotalFareTrueTest() {
+        double totalFare = cabInvoiceGenerator.calculateFare(1.0, 5);
+        Assertions.assertEquals(15.0, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeInvoiceGeneratorShouldReturnMinimumFare() {
+        double totalFare = cabInvoiceGenerator.calculateFare(0.2, 2);
+        Assertions.assertEquals(5, totalFare, 0.0);
+    }
+}
